b79b04676a9dce2333ec387406e55d3758a53313
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-06-23 09:27:18 -0700

    i didnt need browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
-import { BrowserRouter as Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
